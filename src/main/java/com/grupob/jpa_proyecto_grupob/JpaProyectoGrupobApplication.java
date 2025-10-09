215799cfd670616ff179d82db033cce2eb2d672a
IronInkai <i202315741@cibertec.edu.pe>
2025-10-09 17:05:06 -0500

    Inicio

---
+++ src/main/java/com/grupob/jpa_proyecto_grupob/JpaProyectoGrupobApplication.java
@@ -0,0 +1,13 @@
+package com.grupob.jpa_proyecto_grupob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProyectoGrupobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaProyectoGrupobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupob/jpa_proyecto_grupob/JpaProyectoGrupobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupob.jpa_proyecto_grupob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProyectoGrupobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
